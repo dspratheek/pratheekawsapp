38bbdc2ccdfe6f4868cca9f316465dbf2f86404b
Devarinti Sai Pratheek <68936908+dspratheek@users.noreply.github.com>
2022-06-30 04:00:39 +0530

    Add files via upload

---
+++ src/Heading.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Heading(){
+    return <h1>This is Heading</h1>
+}
+export default Heading;
---
+++ src/Para.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+let Para = () => {
+    return <p>This is Paragraph</p>
+}
+
+export default Para;
