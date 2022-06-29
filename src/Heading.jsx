d6db898d1de185a97a0f6d60a40ca3f65c5ff958
Devarinti Sai Pratheek <68936908+dspratheek@users.noreply.github.com>
2022-06-30 04:10:57 +0530

    Delete src directory

--- src/Heading.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-function Heading(){
-    return <h1>This is Heading</h1>
-}
-export default Heading;
--- src/Para.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-let Para = () => {
-    return <p>This is Paragraph</p>
-}
-
-export default Para;
