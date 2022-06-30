572ba3298a8eada23398f6c9efd344079f672d2a
Devarinti Sai Pratheek <68936908+dspratheek@users.noreply.github.com>
2022-06-30 14:37:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p Please wait few seconds after clicking the link to view the content of table. /p>
         <a
           className="App-link"
           href="http://reactapps3pratheek.s3-website-us-east-1.amazonaws.com/"
